3bc405fddd6f63d0935de5b582b5caa32c7e7e0e
Vader1970 <daniel.wilkey@gmail.com>
2024-11-12 15:14:57 +1300

    final updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <p className='text-white-200 md:mt-10 my-5 text-center'>
           Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
         </p>
-        <a href='mailto:contact@jsmastery.pro'>
+        <a href='mailto:michele.carternz@gmail.com'>
           <MagicButton title="Let's get in touch" icon={<FaLocationArrow />} position='right' />
         </a>
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,10 +7,6 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
       <div>
         <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
         <Spotlight className='h-[80vh] w-[50vw] top-10 left-full' fill='purple' />
